90035302873755f0d61eb3e408481ba3dc369cae
2235g3 <cashinhand9033@mail.com>
2026-06-17 00:53:57 +0100

    Added my solution and updated the appropriate READMEs.

--- 20-valid-parentheses/attempt01/README.md
+++ 20-valid-parentheses/attempt01/README.md
@@ -6,4 +6,10 @@ The goal of the problem is to determine whether an input string contains only va
 
 ## Current approach
 
-Currently being implemented.
+Opening parentheses are added to a stack, with the tail being incremented and a valid check boolean being set to false.
+Closing parantheses are verified by comparing the input closing parenthesis to the closing parenthesis associated to the parenthesis at the top of the stack.
+Special cases are dealt with via their own if statements.
+
+## Stats
+*Time:* `2ms` *Beating:* `98.04%`
+*Memory:* `43.03MB` *Beating:* `84.68%`
--- 20-valid-parentheses/attempt01/Solution.java
+++ 20-valid-parentheses/attempt01/Solution.java
@@ -1,5 +1,28 @@
 class Solution {
-	public boolean isValid(String s) {
-		
-	}
+    public boolean isValid(String s) {
+        char[] stack = new char[s.length()];
+        HashMap<Character, Character> map = new HashMap<Character, Character>();
+        map.put('(',')');
+        map.put('{','}');
+        map.put('[',']');
+        int tail = 0;
+        boolean valid = false;
+        for (char p : s.toCharArray()) {
+            if (p == '(' || p == '[' || p == '{') {
+                stack[tail++] = p;
+                valid = false;
+            } else if (tail == 0) {
+                return false;
+            } else if (p != map.get(stack[--tail])) {
+                return false;
+            } else {
+                valid = true;
+            }
+        }
+        if (tail != 0) {
+            return false;
+        }
+        return valid;
+    }
 }
+
